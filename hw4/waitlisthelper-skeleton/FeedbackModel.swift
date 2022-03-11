8b0f4dd8a7b7266d43682e02a428520b9567db1f
nhinguyen26 <nhinguyen1326@yahoo.com>
2022-03-10 20:33:52 -0800

    final submission hw4

---
+++ hw4/waitlisthelper-skeleton/FeedbackModel.swift
@@ -0,0 +1,19 @@
+//
+//  FeedbackModel.swift
+//  waitlisthelper-skeleton
+//
+//  Created by Nhi Nguyen on 3/10/22.
+//
+
+import Foundation
+
+let feedbackModel =
+    [(90, "You're good 😄"),
+     (60, "You're probably okay ☺️"),
+     (40, "Idk, it's like 50-50 😐"),
+     (20, "You're probably not getting off 😕"),
+     (0, "Rip 😔")]
+
+var test = "test"
+
+
