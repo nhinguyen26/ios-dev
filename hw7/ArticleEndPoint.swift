9322535fd9acd8be3fe7153b4a93926288aa23a1
nhinguyen26 <nhinguyen1326@yahoo.com>
2022-04-08 11:01:14 -0700

    final submission

---
+++ hw7/ArticleEndPoint.swift
@@ -0,0 +1,44 @@
+//
+//  ArticleEndPoint.swift
+//  newsfeedSkeleton
+//
+//  Created by Nhi Nguyen on 4/7/22.
+//
+
+import Foundation
+
+protocol APIBuilder {
+    var urlRequest: URLRequest {get}
+    var baseURL: URL {get}
+    var path: String {get}
+    
+}
+
+enum ArticleAPI {
+    case getNews
+}
+
+extension ArticleAPI: APIBuilder {
+    
+    var baseUrl: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getNews:
+            return "/news"
+        }
+    }
+    
+    var urlRequest: URLRequest {
+        switch self {
+        case .getNews:
+            return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+        }
+    }
+    
+}
---
+++ hw7/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  newsfeedSkeleton
+//
+//  Created by Nhi Nguyen on 4/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Screen")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
