0c9955ec7484e14eead897d91a4198bae2206b2d
nhinguyen26 <nhinguyen1326@yahoo.com>
2022-04-14 17:23:33 -0700

    added new project 8

---
+++ Quizlet-ish/Quizlet-ish/Quizlet_ishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quizlet_ishApp.swift
+//  Quizlet-ish
+//
+//  Created by Nhi Nguyen on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Quizlet_ishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
